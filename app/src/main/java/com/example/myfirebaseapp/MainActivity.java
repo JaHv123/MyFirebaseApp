386c9ecc08a3dc858d186dfae781c304b4f029ce
Jimmy <jimmy_hv1@hotmail.com>
2019-06-20 01:26:52 -0500

    Insertar Datos en Firebase

--- app/src/main/java/com/example/myfirebaseapp/MainActivity.java
+++ app/src/main/java/com/example/myfirebaseapp/MainActivity.java
@@ -4,14 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.myfirebaseapp.Models.Persona;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText nombrePersona, apellidosPersona,correoPersona,passwordPersona;
+    ListView listVPersona;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombrePersona = findViewById(R.id.txt_NombrePersona);
+        apellidosPersona = findViewById(R.id.txt_apellidosPersona);
+        correoPersona = findViewById(R.id.txt_correoPersona);
+        passwordPersona = findViewById(R.id.txt_passwordPersona);
+
+        listVPersona = findViewById(R.id.lv_datosPersona);
+
+        inicializarFirebase();
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
     }
 
     @Override
@@ -20,22 +50,44 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
+        String nombre = nombrePersona.getText().toString();
+        String apellidos = apellidosPersona.getText().toString();
+        String correo = correoPersona.getText().toString();
+        String password = passwordPersona.getText().toString();
+
+
         switch (item.getItemId()){
             case R.id.icon_add:{
-                Toast.makeText(this,"Agregar",Toast.LENGTH_SHORT).show();
+                if (nombre.equals("") || apellidos.equals("") || correo.equals("") || password.equals("")){
+                    validacion();
+                }else {
+                    Persona p = new Persona();
+                    p.setId(UUID.randomUUID().toString());
+                    p.setNombre(nombre);
+                    p.setApellidos(apellidos);
+                    p.setCorreo(correo);
+                    p.setPassword(password);
+
+                    databaseReference.child("Persona").child(p.getId()).setValue(p);
+
+                    Toast.makeText(this,"Agregado",Toast.LENGTH_SHORT).show();
+                    limpiarCajas();
+                }
                 break;
             }
 
             case R.id.icon_save:{
-                Toast.makeText(this,"Guardar",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this,"Actualizado",Toast.LENGTH_SHORT).show();
                 break;
             }
 
             case R.id.icon_delete:{
-                Toast.makeText(this,"Eliminar",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this,"Eliminado",Toast.LENGTH_SHORT).show();
                 break;
             }
 
@@ -43,5 +95,30 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return true;
+    }
+
+    private void limpiarCajas() {
+        nombrePersona.setText("");
+        apellidosPersona.setText("");
+        correoPersona.setText("");
+        passwordPersona.setText("");
+    }
+
+    private void validacion() {
+        String nombre = nombrePersona.getText().toString();
+        String apellidos = apellidosPersona.getText().toString();
+        String correo = correoPersona.getText().toString();
+        String password = passwordPersona.getText().toString();
+
+
+        if (nombre.equals("")){
+            nombrePersona.setError("Required");
+        }else if (apellidos.equals("")){
+            apellidosPersona.setError("Required");
+        }else if (correo.equals("")){
+            correoPersona.setError("Required");
+        }else if (password.equals("")){
+            passwordPersona.setError("Required");
+        }
     }
 }
