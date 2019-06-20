b90b811af36ad79dcfced39d7d207157e3351046
Jimmy <jimmy_hv1@hotmail.com>
2019-06-19 22:28:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myfirebaseapp/Models/Persona.java
@@ -0,0 +1,49 @@
+package com.example.myfirebaseapp.Models;
+
+public class Persona {
+
+    public String Id;
+    public String Nombre;
+    public String Apellidos;
+    public String Correo;
+
+    public Persona() {
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return Nombre;
+    }
+}
